71bffc04c6e889c5191a04f2e3bfcc98b2dd05ba
root <root@master.example.com>
2022-04-04 06:08:46 +0000

    01-Docker

---
+++ 01-Docker/00-Setup/install-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "###################### Installing docker ########################"
+sleep 2 
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+apt-get update && apt-get install -y docker-ce
+
+
